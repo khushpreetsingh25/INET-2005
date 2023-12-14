5ffd7f785ab83d792799dba187ea713c70954f2d
khushpreetsingh25 <114405425+khushpreetsingh25@users.noreply.github.com>
2023-12-14 15:50:48 -0400

    Initial commit of final project

---
+++ Final-Project/inventory-management/resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your App Name</title>
+
+    <!-- Include Bootstrap CSS (CDN) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-3">
+        @yield('content')
+    </div>
+
+    <!-- Include Bootstrap JS (CDN) if needed -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ Final-Project/inventory-management/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+
+
+
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::patch('/categories/{id}', [CategoryController::class, 'update'])->name('categories.update');
+
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
+Route::post('/items', [ItemController::class, 'store'])->name('items.store');
+Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
+Route::patch('/items/{id}', [ItemController::class, 'update'])->name('items.update');
+Route::delete('/items/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
+
+Route::get('/', function () {
+    return view('welcome');
+});
